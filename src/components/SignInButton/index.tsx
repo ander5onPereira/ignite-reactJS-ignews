f89332b1d33204f5c6ffc3a37b101c725c43c9ba
Anderson Pereira <anderson.carro.95@gmail.com>
2022-03-29 23:14:15 -0300

    Chapter III - Componentes e páginasEstrutura da aplicação - Página Home

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="#04d361" /> User Name
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => setIsUserLoggedIn(!isUserLoggedIn)}
+    >
+      <FaGithub color="#04d361" />
+      User Name
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="#eba417" /> Sign in with Github
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => setIsUserLoggedIn(!isUserLoggedIn)}
+    >
+      <FaGithub color="#eba417" />
+      Sign in with Github
     </button>
   );
 }

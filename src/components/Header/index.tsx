bc7b2d7b23003e1023c9801315b47343b14ebbde
Anderson Pereira <anderson.carro.95@gmail.com>
2022-08-01 12:32:51 -0300

    Chapter III - Front-end JAMStack - Páginas estáticas - Página: Post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
@@ -5,7 +6,12 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image
+          src="/images/logo.svg"
+          alt="ig.news"
+          width={'100%'}
+          height={'100%'}
+        />
         <nav>
           <ActiveLink activeClassName={styles.active} href="/">
             <a>Home</a>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 import * as Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -24,11 +25,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
@@ -46,6 +49,7 @@ export const getStaticProps: GetStaticProps = async () => {
       pageSize: 100,
     },
   );
+  console.log({ response });
   const posts = response.results.map((post) => {
     /* 
       DICA de desempenho
